25aab59561d8a321c6d7ffff2ace93acad347539
nevimjaksejmenovat <mikurecka@gmail.com>
2021-01-11 18:39:21 +0100

    otazky + odpovedi anglicky

---
+++ models/question.py
@@ -0,0 +1,26 @@
+class Question:
+    def __init__(self):
+        self.question = "default q"
+        self.answer = "default a"
+
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+    @property
+    def question(self):
+        return self.__question
+
+    @question.setter
+    def question(self, value):
+        self.__question = value
+
+    @property
+    def answer(self):
+        return self.__answer
+
+    @answer.setter
+    def answer(self, value):
+        self.__answer = value
+
+
