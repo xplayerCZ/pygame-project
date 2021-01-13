1cb4686629930fea9cb4d86d7e496738e783934c
nevimjaksejmenovat <mikurecka@gmail.com>
2021-01-13 12:34:05 +0100

    otazky + odpovedi anglicky

--- crossword.py
+++ crossword.py
@@ -1,3 +1,31 @@
 from models import game
+import pygame as pg
 
-game.Crossword.generate("words.csv")
+clock = pg.time.Clock()
+class Main:
+    answers = [["P", "A", "w"], ["B", "C", "D", "E"], ["E", "b"]]
+    input_boxes = []
+    for i in range(len(answers)):
+        for j in range(len(answers[i])):
+            if answers[i][j]:
+                input_boxes.append(game.Box(100 + j * game.CUBE, 25 + i * game.CUBE, game.CUBE, game.CUBE))
+
+    done = False
+
+    while not done:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+            for box in input_boxes:
+                box.on_click(event)
+
+        for box in input_boxes:
+            box.update()
+
+        game.screen.fill((255, 255, 255))
+        for box in input_boxes:
+            box.draw(game.screen)
+
+        pg.display.flip()
+        clock.tick(30)
+
--- models/game.py
+++ models/game.py
@@ -1,20 +1,51 @@
-from models import question
-import random
+import pygame as pg
 
-class Crossword:
-    def __init__(self):
-        rows = []
+WIDTH = 640
+HEIGHT = 480
+CUBE = 50
+VELIKOST_PISMA = 27
 
-    @staticmethod
-    def generate(question_file):
-        with open(question_file) as f:
-            lines = [question.Question(i.split(";")[0], i.split(";")[1]) for i in f.readlines()]
-            word_puzzle = random.choice(lines)
-        splited_puzzle = list(word_puzzle.answer)
-        splited_puzzle = splited_puzzle[:-1]
+pg.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+COLOR_NONE = pg.Color('black')
+COLOR_WRITE = pg.Color('red')
+FONT = pg.font.SysFont("Arial", VELIKOST_PISMA)
 
-        for x in splited_puzzle:
-            random_fill = random.choice(lines)
-            split_fill = list(random_fill.answer)
-            print(x, split_fill)
+
+class Box:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pg.Rect(x, y, w, h)
+        self.color = COLOR_NONE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def on_click(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.active = not self.active
+            else:
+                self.active = False
+            self.color = COLOR_WRITE if self.active else COLOR_NONE
+
+        if event.type == pg.KEYDOWN:
+            if self.active:
+                if event.key == pg.K_RETURN:
+                        print(self.text)
+                        self.text = ''
+                elif event.key == pg.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    while len(self.text) != 1:
+                        self.text += event.unicode
+                self.txt_surface = FONT.render(self.text.upper(), True, self.color)
+
+    def update(self):
+        width = max(CUBE, self.txt_surface.get_width())
+        self.rect.w = width
+
+    def draw(self, screen):
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        pg.draw.rect(screen, self.color, self.rect, 2)
 
