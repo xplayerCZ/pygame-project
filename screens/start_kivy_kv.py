cede774fe3cd1adf6d5c651c5b7cf3d0a27e318f
nevimjaksejmenovat <mikurecka@gmail.com>
2021-02-20 16:15:03 +0100

    Pridane: vyber jmena, ktere se pak vypise do "NAME", tlacitka funguji, Label text pridany 2x.
    To do: vypinani aplikaci nefunguje

--- screens/start_kivy_kv.py
+++ screens/start_kivy_kv.py
@@ -8,11 +8,13 @@ SOUBOR = "crossword.xlt"
 NAZEV_SOUBORU = "Choose File"
 
 
+
 class FloatLayout(FloatLayout):
     Window.clearcolor = (255, 255, 255, 255)
     pass
 
 class ButtonApp2(App):
+    NAME = "default player"
 
     def build(self):
         self.soubor = SOUBOR
@@ -21,6 +23,7 @@ class ButtonApp2(App):
     def turnOn(self, event):
         start.stop()
         print(SOUBOR)
+        print(ButtonApp2.NAME)
         from models import kivy_filestart
         kivy_filestart.First.Second(self, self.soubor)
         start.stop()
@@ -28,6 +31,10 @@ class ButtonApp2(App):
     def chooseFile(self, event):
         self.soubor = filedialog.askopenfilename()
         print(SOUBOR)
+
+    def enteredName(self):
+        text = self.root.ids.input.text
+        ButtonApp2.NAME = text
 
 start = ButtonApp2()
 start.run()
